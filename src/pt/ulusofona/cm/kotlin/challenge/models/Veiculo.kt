6b1abe3050a15aa1f3ebd73713f2e5c732010d4b
sofia-22104464 <91740320+sofia-22104464@users.noreply.github.com>
2023-03-27 15:47:19 +0100

    Add files via upload

--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -7,7 +7,7 @@ import java.util.*
 open class Veiculo(var identificador: String): Movimentavel {
     var posicao= Posicao(0,0)
     //var dataDeAquisicao: Date= Date(LocalDate.now().year,LocalDate.now().monthValue,LocalDate.now().dayOfMonth)
-    var dataDeAquisicao: Date= Date(0,0,0)
+    var dataDeAquisicao: Date= Date(0,1,0)
 
     open fun requerCarta(): Boolean {
         return true
